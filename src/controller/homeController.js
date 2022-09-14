bb5c8042c195c3e7ab375f129bab2eab31e1637e
@anhnv99 <vietanhhappy99@gmail.com>
2022-09-14 15:35:24 +0700

    #13 COMPLETE CRUD

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -20,6 +20,28 @@ let createNewUser = async(req, res) => {
     [firstName, lastName, email, address]);
     return res.redirect('/')
 }
+
+let deleteUser = async(req, res) => {
+    let userId = req.body.userId;
+    // DELETE FROM table_name WHERE condition;
+    await pool.execute('delete from users where id = ?', [userId])
+    return res.redirect('/')
+}
+
+let getEditPage = async(req, res) => {
+    let id = req.params.id;
+    let [user] = await pool.execute('select * from users where id = ?', [id]);
+    return res.render('update.ejs', {dataUser: user[0]}); // x <- y
+
+}
+let postUpdateUser = async (req, res) => {
+    let {firstName, lastName, email, address, id} = req.body;
+    // UPDATE table_name SET column1 = value1, column2 = value2, ... WHERE condition;
+    await pool.execute('update users set firstName = ?, lastName = ?, email = ?, address = ? where id = ?',
+     [firstName, lastName, email, address, id]);
+    return res.redirect('/');
+
+}
 module.exports = {
-    getHomePage, getDetailPage, createNewUser
+    getHomePage, getDetailPage, createNewUser, deleteUser, getEditPage, postUpdateUser
 }
--- src/route/web.js
+++ src/route/web.js
@@ -6,9 +6,12 @@ let router = express.Router();
 
 const initWebRoute = (app) => {
     
-    router.get('/', homeController.getHomePage)
-    router.get('/detail/user/:id', homeController.getDetailPage)
-    router.post('/create-new-user', homeController.createNewUser)
+    router.get('/', homeController.getHomePage);
+    router.get('/detail/user/:id', homeController.getDetailPage);
+    router.post('/create-new-user', homeController.createNewUser);
+    router.post('/delete-user', homeController.deleteUser);
+    router.get('/edit-user/:id', homeController.getEditPage);
+    router.post('/update-user', homeController.postUpdateUser)
 
     router.get('/test', (req, res) => {
         res.render('test/index.ejs')
