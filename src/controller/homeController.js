83da75db75cfb581987374b8d6ddfe41fe4b1ad1
@anhnv99 <vietanhhappy99@gmail.com>
2022-09-23 11:27:21 +0700

    update

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -3,21 +3,21 @@ import multer from 'multer';
 
 
 let getHomePage = async (req, res) => {
+    
+    // The SQL SELECT Statement || SQL SELECT
     const [rows, fields] = await pool.execute('SELECT * FROM `users`', );
     return res.render('index.ejs', {dataUser: rows, test: "abc string test"})
 
 }
 let getDetailPage = async(req, res) => {
-    // console.log('check request', req.params)
+    // The SQL WHERE Clause || SQL WHERE Clause
     let userId = req.params.id;
     let [user] = await pool.execute(`select * from users where id = ?`, [userId])
     console.log('check request', user)
     return res.send(JSON.stringify(user))
 }
 let createNewUser = async(req, res) => {
-    console.log('check req: ', req.body)
-    // INSERT INTO table_name (column1, column2, column3, ...)
-    // VALUES (value1, value2, value3, ...);
+    // The SQL INSERT INTO Statement || SQL INSERT
     let {firstName, lastName, email, address} = req.body;
     await pool.execute('insert into users(firstName, lastName, email, address) values (?, ?, ?, ?)', 
     [firstName, lastName, email, address]);
@@ -25,19 +25,21 @@ let createNewUser = async(req, res) => {
 }
 
 let deleteUser = async(req, res) => {
+    // The SQL DELETE Statement || SQL DELETE
     let userId = req.body.userId;
-    // DELETE FROM table_name WHERE condition;
     await pool.execute('delete from users where id = ?', [userId])
     return res.redirect('/')
 }
 
 let getEditPage = async(req, res) => {
+    // The SQL WHERE Clause || SQL WHERE Clause
     let id = req.params.id;
     let [user] = await pool.execute('select * from users where id = ?', [id]);
     return res.render('update.ejs', {dataUser: user[0]}); // x <- y
 
 }
 let postUpdateUser = async (req, res) => {
+    // The SQL UPDATE Statement
     let {firstName, lastName, email, address, id} = req.body;
     // UPDATE table_name SET column1 = value1, column2 = value2, ... WHERE condition;
     await pool.execute('update users set firstName = ?, lastName = ?, email = ?, address = ? where id = ?',
